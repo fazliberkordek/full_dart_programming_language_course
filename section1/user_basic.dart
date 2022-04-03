f621b9dba352779eda04df84b9cdaa904d7307cf
FazlI Berk ORDEK <berkordek97@gmail.com>
2022-04-04 01:54:33 +0300

    user_basic.dart includes basic alghortim exercise

--- section1/user_basic.dart
+++ section1/user_basic.dart
@@ -1,3 +1,18 @@
 void main() {
+  //todo: I want to hold the customers name /
+   //todo: Holds customers budget
+   // todo: Welocme the Customer with his/her name
+   // todo: and add +5 volume to his bugdet for choosing our 
+
+   String UserName = 'John Smith';
+   int UserBudget = 100;
+   print('Welcome $UserName; You have: $UserBudget money'); //! Prints the customer name and his budget
+   print('Thanks for choosing our bank we will add bonus welcome profit to your account'); //! Prints the message
+   UserBudget = UserBudget + 5;//! Adds 5 to the customers budget
+   print('Dear $UserName your new budget is : $UserBudget');//! Prints the new budget
+
+
+
+
   
 }
