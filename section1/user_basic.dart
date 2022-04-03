a23a39dfe7be244e1f69d10b674d692cc2fbcb04
FazlI Berk ORDEK <berkordek97@gmail.com>
2022-04-04 01:58:46 +0300

    Did simple exercise

--- section1/user_basic.dart
+++ section1/user_basic.dart
@@ -1,3 +1,23 @@
 void main() {
+  //todo: I want to hold the customers name /
+   //todo: Holds customers budget
+   // todo: Welocme the Customer with his/her name
+   // todo: and add +5 volume to his bugdet for choosing our 
+
+   String UserName = 'John Smith';
+   int UserBudget = 100;
+   
+   //! Prints the customer name and his budget
+   print('Welcome $UserName; You have: $UserBudget money'); 
+   //! Prints the message
+   print('Thanks for choosing our bank we will add bonus welcome profit to your account'); 
+   //! Adds 5 to the customers budget
+   UserBudget = UserBudget + 5;
+   //! Prints the new budget
+   print('Dear $UserName your new budget is : $UserBudget');
+
+
+
+
   
 }
