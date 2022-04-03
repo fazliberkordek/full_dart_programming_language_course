d6359c81280e8822dfa4d499e6912f5d11982811
FazlI Berk ORDEK <berkordek97@gmail.com>
2022-04-04 01:28:11 +0300

    user_basic.dart has created inside of section 1

--- section1/start.dart
+++ section1/start.dart
@@ -1,3 +1,33 @@
-void void main() {
-  print('Hello, World!');
+void main() {
+// prin fucntions as a display method to performs like as its named for:
+// Deport the objectt inside of it and display it.
+  print("Hello"+ " World"); //  you can use + to concatenate strings.  
+
+  print(2+3); // you can use + to add numbers.  
+  print('Hello World!');
+  // now we can  learn variables.
+  
+  String hello = 'Hello5' ; // Srring values_name = 'value'.
+  print(hello); // firs Hello World2
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+
+  // Ig=f you want to define integer variable you can use: int variable name = value;
+  int money = 31;
+  print(money);
+
+  //for example if you want to print hello and money togather in one line you can use: 
+  print('Hello' + money.toString()); // you can use .toString() to convert the integer to string.
+  //or you can use:
+  print('Hello $money'); // you can use $money to display the integer value.
+  //or you can use:
+  print('Hello ${money}'); // you can use ${money} to display the integer value.
+
+
+
+
+
 }
+
